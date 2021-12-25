cd459f74849af7e048522a53c449d829a74f0c5a
Martin <martin.tomaskovic1@gmail.com>
2021-12-25 13:59:48 +0100

    An example with submodules.

--- build/CMakeFiles/tutorial.dir/cmake_clean.cmake
+++ build/CMakeFiles/tutorial.dir/cmake_clean.cmake
@@ -1,10 +1,11 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/tutorial.dir/src/glad.c.o"
   "CMakeFiles/tutorial.dir/src/main.cpp.o"
   "tutorial"
   "tutorial.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
