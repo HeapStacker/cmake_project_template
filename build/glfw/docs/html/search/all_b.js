386ae34ca8fe93b09097e12b17e3069b3d2060f3
Martin <martin.tomaskovic1@gmail.com>
2021-12-25 14:18:56 +0100

    no build

--- build/glfw/docs/html/search/all_b.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['native_20access_547',['Native access',['../group__native.html',1,'']]],
-  ['news_2edox_548',['news.dox',['../news_8dox.html',1,'']]],
-  ['notitle_549',['notitle',['../index.html',1,'']]]
-];
